eb2c7d42e6e364bbb7edc17801ff1380d0918ed5
Sun, Susan <susan.sun@capgemini.com>
2020-11-16 13:01:16 +0000

    create dead letter queue construct

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,24 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as sns from '@aws-cdk/aws-sns';
+import * as sqs from '@aws-cdk/aws-sqs';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
 export interface DeadLetterQueueProps {
-  // Define construct properties here
+  visibilityTimeout?: cdk.Duration;
 }
 
 export class DeadLetterQueue extends cdk.Construct {
-
+  public readonly queueArn:  string;
   constructor(scope: cdk.Construct, id: string, props: DeadLetterQueueProps = {}) {
     super(scope, id);
 
-    // Define construct contents here
+    const queue = new sqs.Queue(this, 'DeadLetterQueueQueue', {
+      visibilityTimeout: props.visibilityTimeout || cdk.Duration.seconds(300)
+    })
+    
+    const topic = new sns.Topic(this, 'DeadLetterQueueTopic'); 
+
+    topic.addSubscription(new subs.SqsSubscription(queue))
+
+    this.queueArn = queue.queueArn;
   }
 }
